9f1860bf01979a51768964db5893efaac6b79887
deakzsolt <deakzolt@gmail.com>
2023-11-15 21:29:00 +0100

    Update files
    create readme file
    code cleanup

--- app/Http/Controllers/Web/PersonController.php
+++ app/Http/Controllers/Web/PersonController.php
@@ -13,7 +13,6 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Application as FoundationApplication;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
